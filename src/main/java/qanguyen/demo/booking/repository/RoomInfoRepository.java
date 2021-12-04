511211d1b1526a1fd1388bbd2d5197b9b5342933
Quoc Anh Nguyen <quoc.anh.nguyen@mgm-tp.com>
2021-12-04 10:48:56 +0700

    Add UI using Vue js and adapt deployment

--- src/main/java/qanguyen/demo/booking/repository/RoomInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package qanguyen.demo.booking.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import qanguyen.demo.booking.entity.RoomInfo;
-
-@Repository
-public interface RoomInfoRepository extends CrudRepository<RoomInfo, Long> {
-}
